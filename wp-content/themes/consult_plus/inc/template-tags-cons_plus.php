bd09f98a1808d103eb9898f96c3ec2baaaca444a
im <maryna.todorishyna@ekreative.com>
2017-07-31 11:01:16 +0300

    Added styles for single post

--- wp-content/themes/consult_plus/inc/template-tags-cons_plus.php
+++ wp-content/themes/consult_plus/inc/template-tags-cons_plus.php
@@ -47,6 +47,45 @@ if ( ! function_exists( 'consult_plus_posted_on' ) ) :
 	}
 endif;
 
+if ( ! function_exists( 'consult_plus_posted_on_single' ) ) :
+	/**
+	 * Prints HTML with meta information for the current post-date/time and author.
+	 */
+	function consult_plus_posted_on_single() {
+
+
+
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		}
+
+		$time_string = sprintf( $time_string,
+			esc_attr( get_the_date( 'Y/M/d' ) ),
+			esc_html( get_the_date('d-M-Y') ),
+			esc_attr( get_the_modified_date( 'Y/M/d' ) ),
+			esc_html( get_the_modified_date() )
+		);
+
+		$posted_on = sprintf(
+		/* translators: %s: post date. */
+			esc_html_x( '%s', 'post date', 'consult_plus' ),
+			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		);
+
+		$byline = sprintf(
+		/* translators: %s: post author. */
+			esc_html_x( 'by %s', 'post author', 'consult_plus' ),
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		);
+
+		echo  $posted_on; // WPCS: XSS OK.
+
+	}
+endif;
+
+
+
 if ( ! function_exists( 'consult_plus_entry_footer' ) ) :
 	/**
 	 * Prints HTML with meta information for the categories, tags and comments.
---
+++ wp-content/themes/consult_plus/single-consultPlus.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Template Name: Single Post Page
+ * Template Post Type: post, page, product
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main m-container">
+
+		<?php
+		while ( have_posts() ) : the_post();
+
+			get_template_part( 'template-parts/content', get_post_format() );
+
+			the_post_navigation();
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
